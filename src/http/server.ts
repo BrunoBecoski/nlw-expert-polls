1a40ad9e197c2a465efe7d4e220e0bcbe7501a22
Bruno Becoski <brunobecoski@gmail.com>
2024-02-09 20:39:26 -0300

    Cria a rota Obter Enquete

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,12 @@
 import fastify from 'fastify';
 
 import { createPoll } from './rotes/create-poll';
+import { getPoll } from './rotes/get-poll';
 
 const app = fastify();
 
-app.register(createPoll)
+app.register(createPoll);
+app.register(getPoll);
 
 app.listen({ port: 3333}).then(() => {
   console.log('🚀 HTTP server running');
