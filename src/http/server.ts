c233ac13a940b07f6f23737533cd4e7e8e034c1d
Bruno Becoski <brunobecoski@gmail.com>
2024-02-09 17:03:00 -0300

    Cria o banco de dados

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,1 +1,12 @@
-console.log('Hello NLW');
+import fastify from 'fastify';
+
+const app = fastify();
+
+app.get('/polls', () => {
+  return 'Hello NLW';
+})
+
+app.listen({ port: 3333}).then(() => {
+  console.log('🚀 HTTP server running');
+})
+
