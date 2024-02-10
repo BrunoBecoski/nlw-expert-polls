5949e69dc1c09652c5a42bfdfe33648391477c3a
Bruno Becoski <brunobecoski@gmail.com>
2024-02-09 22:04:04 -0300

    Cria a rota Votar na Enquete

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,14 +1,21 @@
 import fastify from 'fastify';
+import cookie from '@fastify/cookie';
 
 import { createPoll } from './rotes/create-poll';
 import { getPoll } from './rotes/get-poll';
+import { voteOnPoll } from './rotes/vote-on-poll';
 
 const app = fastify();
 
+app.register(cookie, {
+  secret: 'poll-app-nlw',
+  hook: 'onRequest',
+});
+
 app.register(createPoll);
 app.register(getPoll);
+app.register(voteOnPoll);
 
 app.listen({ port: 3333}).then(() => {
   console.log('🚀 HTTP server running');
 });
-
